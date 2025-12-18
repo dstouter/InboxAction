3b048b824e908020266b4f4be410e7120aa29eda
Jacksipps <jsipperly16@gmail.com>
2025-12-18 02:22:26 -0500

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { ReactNode } from "react";
 import Providers from "./providers";
 
-export const metadata = {
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
 export default function RootLayout({
   children,
 }: {
