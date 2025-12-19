2e071d123d6bac346ea4cb7b75c27bee28e36db3
Jacksipps <jsipperly16@gmail.com>
2025-12-18 21:32:48 -0500

    Add metadata for favicon in layout component

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { ReactNode } from "react";
 import Providers from "./providers";
 
+export const metadata = {
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
